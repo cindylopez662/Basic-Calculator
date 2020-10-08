5a7fff7e6c4bf87eed9c53e03af6417d000b7f78
cindylopez662 <cindy.lopez705@gmail.com>
2020-10-07 21:09:36 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 #functions, baby
+#will not accept decimals 
 '''
 simple calculator
 to practice functions and ifs 
@@ -17,7 +18,24 @@ def multiply(num1, num2):
 def divide(num1, num2):
     quotient = num1 / num2
     return quotient
-
+#variables can be updated
+def get_numbers():
+    is_number = False 
+    while not is_number:
+        num_answer = input("Which two numbers do you want to use? ").split(" ")
+        try:
+            num1 = int(num_answer[0])
+            num2 = int(num_answer[1])
+        except:
+            try:
+                num1 = float(num_answer[0])
+                num2 = float(num_answer[1])
+            except:
+                print("These are not numbers.")
+                continue
+        is_number = True
+    return [num1, num2]
+#function is going to prompt the user for two numbers - verify that they are numbers - return the actual numbers
 #to do: verify that u entered a number, optimize the num conversion, (allow decimals?)
 
 if __name__ == '__main__':
@@ -29,13 +47,9 @@ if __name__ == '__main__':
                 break
             if answer not in questions:
                 print("That's not a valid answer.")
-            num_answer = input("Which two numbers do you want to use? ").split(" ")
-
-            if num_answer != int(num_answer[0], int(num_answer[1])):
-                print("This is not a number")
-            if num_answer != float(num_answer[0], float(num_answer[1])):
-                print("This is not a number")
-                
+            num_answer = get_numbers()
+            if not num_answer:
+                continue
             if answer == 'add':
                 add_answer = adding(int(num_answer[0]),int(num_answer[1]))
                 print("You answer is", add_answer)
